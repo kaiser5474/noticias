b5b96f02f1f5be6f7adee208d118e77776a0e7e9
Jarvis <jarvismiranda@gmail.com>
2022-02-21 17:17:27 -0500

    agregando estilos a noticia

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Noticia.module.css";
 
 const Noticia = ({ noticia }) => {
   const { author, description, urlToImage, source, url, title } = noticia;
@@ -6,11 +7,22 @@ const Noticia = ({ noticia }) => {
     <div className="col s12 m6 l4">
       <div className="card">
         <div className="card-image">
-          <img src={urlToImage} alt={title  } />
+          <img src={urlToImage} alt={title} />
+          <span className="card-title">{source.name}</span>
         </div>
         <div className="card-content">
           <h4>{title}</h4>
-          <p>{description}</p>
+          <p className={styles.resumen}>{description}</p>
+        </div>
+        <div className={`card-action ${styles.botonIrNoticia}`}>
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="waves-effect waves-light btn"
+          >
+            Ver Noticia completa
+          </a>
         </div>
       </div>
     </div>
